4712330398f2f0b299bcb29bdb1e94d13d3afd0b
Santiago233 <468102981qq@gmail.com>
2021-10-22 17:31:43 +0800

    client_linux revise

---
+++ client_linux_mac/Reversi.h
@@ -0,0 +1,56 @@
+//
+//  Reversi.h
+//  ReversiClient
+//
+//  Created by ganjun on 2018/3/6.
+//  Copyright © 2018年 ganjun. All rights reserved.
+//
+
+#ifndef Reversi_h
+#define Reversi_h
+#include <stdio.h>
+#include "ClientSocket.h"
+
+using namespace std;
+
+class Reversi{
+private:
+    ClientSocket client_socket;
+    int ownColor;
+    int oppositeColor;
+    char lastmsg[16];
+public:
+    Reversi();
+    ~Reversi();
+    
+    void authorize(const char *id , const char *pass);
+    
+    void gameStart();
+    
+    void gameOver();
+    
+    void roundStart(int round);
+    
+    void oneRound();
+    
+    void roundOver(int round);
+    
+    int observe();
+    
+    void putDown(int row , int col);
+    
+    // according to chessman position (row , col) , generate one step message in order to send to server
+    void generateOneStepMessage(int row , int col);
+    
+    void noStep();
+    
+    pair<int,int> step();
+    
+    void saveChessBoard();
+    
+    void debug_lastmsg();
+    
+    void handleMessage(int row, int col, int color);
+};
+
+#endif /* Reversi_h */
